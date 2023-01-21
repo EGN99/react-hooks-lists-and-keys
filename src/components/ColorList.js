a741767d7554dc4fd7fa1ded8b79baf2e6100c00
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-01-21 12:56:16 +0300

    completed lab

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+function ColorItem(props) {
+  return <li style={{color:props.color}}>{props.color}</li>
+}
+
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -10,8 +15,9 @@ function ColorList() {
   ];
   
   const colorElements = colors.map((color)=>{
-    return <li key={color} style={{color:color}}>{color}</li>
+    return <ColorItem key={color} color={color}/>
   });
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
